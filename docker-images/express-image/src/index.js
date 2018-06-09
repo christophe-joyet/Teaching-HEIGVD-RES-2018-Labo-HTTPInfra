0f4d61a78bc94ae1a9e811820837703fc860a46a
Christophe Joyet <christophe.joyet@heig-vd.ch>
2018-06-09 15:36:23 +0200

    add HTML dynamic part 2a-b Labo HTTP infa

---
+++ docker-images/express-image/src/index.js
@@ -0,0 +1,44 @@
+var Chance = require('chance');
+var chance = new Chance();
+
+var express = require('express');
+var app = express();
+
+//on regarde si l'on reçoit une requête GET avec / comme argument + HTTP/1.0
+// le / est la ressource visée
+app.get('/', function(req, res) {
+	res.send(generateStudents());
+});
+
+//On écoute sur le port 9090
+app.listen(3000, function(){
+	console.log("Listen HTTP request on port 9090");
+});
+
+function generateStudents() {
+	var numberOfStudents = chance.integer({
+	 min:0,
+	 max:10
+	});
+	console.log(numberOfStudents);
+	var students = [];
+	for (var i = 0; i < numberOfStudents; ++i) {
+	 var gender = chance.gender();
+	 var birthYear = chance.year({
+		min: 1086,
+		max: 1996
+ 	 });
+	 students.push({
+	  fistName: chance.first({
+	   gender: gender
+	  }),
+	  lastName: chance.last(),
+	  gender: gender,
+	  birthday: chance.birthday({
+	   year: birthYear
+	  })
+	 });
+	};
+console.log(students);
+return students;
+}
